847c2671041b1c2c81217cb4f7f2b1f393abf9f8
Serge Rey <sjsrey@gmail.com>
2013-09-06 15:57:05 -0700

    - simple k function

--- pysal/esda/points.py
+++ pysal/esda/points.py
@@ -1,5 +1,7 @@
 import pysal as ps
 
+
+
 def mbr(points):
 
     """
@@ -19,8 +21,8 @@ def mbr(points):
     """
 
     #pl = ps.cg.locators.PointLocator([ps.cg.shapes.Point(p) for p in points])
-    maxc = points.max(axis=1)
-    minc = points.min(axis=1)
+    maxc = points.max(axis=0)
+    minc = points.min(axis=0)
 
     return [minc[0],minc[1], maxc[0], maxc[1]] 
 
@@ -36,7 +38,9 @@ class PointsCollection(object):
         self._area = None
         self._density = None
         self._mbr = None
+        self._mtd = None
         window = self.set_window(window)
+        self.points = points
         self.locator = ps.cg.locators.PointLocator([ps.cg.shapes.Point(p) for
             p in points])
 
@@ -51,6 +55,11 @@ class PointsCollection(object):
         self._density = self._n / self._area
 
     window = property(get_window, set_window)
+
+    def get_n(self):
+        return self._n
+
+    n = property(get_n)
 
     def get_area(self):
         return self._area
@@ -69,17 +78,59 @@ class PointsCollection(object):
 
     mbr = property(get_mbr)
 
+    def get_max_theoretical_distance(self):
+        if self._mtd is None:
+            if self._mbr is None:
+                self._mbr = ps.cg.shapes.Rectangle(*mbr(points))
+            dx = self._mbr.right - self._mbr.left
+            dy = self._mbr.upper - self._mbr.lower
+            self._mtd = np.sqrt(dx*dx + dy*dy)
+        return self._mtd
+
+    mtd = property(get_max_theoretical_distance)
 
 
 
-    
+def k(points, n_bins=10, delta=1.00001):
+    if not isinstance(points, PointsCollection):
+        points = PointsCollection(points)
+    width = points.mtd / n_bins
+    print points.mtd
+    bins = np.arange(0, pc.mtd+width, width)
+    bins[-1] *= delta # to have max contained
+    counts = np.zeros(len(bins)-1,)
+    print len(counts)
+
+    pairs = 0
+    maxd = 0
+    # brute force distances
+    for i in xrange(points.n-1):
+        p_i = points.points[i]
+        dx = points.points[i,0] - points.points[:,0]
+        dy = points.points[i,1] - points.points[:,1]
+        d_i = np.sqrt(dx*dx + dy*dy)
+        print len(d_i)
+        pairs += len(d_i)
+        counts_i = np.histogram(d_i, bins)[0]
+        counts = counts + counts_i
+        maxd = max(maxd, max(d_i))
+        #print len(counts_i)
+
+    print pairs
+    print maxd
+
+
+
+    return bins, points.mtd, counts
+
+
 
 
 if __name__ == '__main__':
 
     import numpy as np
 
-    points = np.random.random((50,2))
+    points = np.random.random((50,2))*10
 
     points_mbr = mbr(points)
 
